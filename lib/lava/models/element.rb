e82de9ac4eef4c1be9540b27daa934c160e10529
Gavin Laking <gavinlaking@gmail.com>
2012-05-01 15:20:33 +0100

    fixed: attr_accessible

--- lib/lava/models/element.rb
+++ lib/lava/models/element.rb
@@ -4,16 +4,19 @@ module Lava
       extend ActiveSupport::Concern
 
       included do
+        attr_accessible :element_type, :reference, :value, :height, :width, :upload
+        attr_accessor :width, :height
+        
         validates_presence_of :reference
 
         file_accessor :upload
         
         default_scope :order => "created_at DESC"
 
-        attr_accessor :width, :height
-
         def self.find_or_create_element(element_attributes)
-          element = self.find(:first, :conditions => {:reference => element_attributes[:reference], :element_type => element_attributes[:element_type]})
+          element = self.where({
+            :reference    => element_attributes[:reference],
+            :element_type => element_attributes[:element_type]}).first
           if element.nil?
             element = self.create(element_attributes)
           end
