1e3602e69c5e8d03e5e87c6aff22c29057e9090e
Gavin Laking <gavinlaking@gmail.com>
2012-05-01 14:28:35 +0100

    fixed: migration

--- lib/generators/lava/templates/migrations/elements.rb
+++ lib/generators/lava/templates/migrations/elements.rb
@@ -1,15 +1,15 @@
 class CreateElementsTable < ActiveRecord::Migration
   def self.up
     create_table :elements, :force => true do |t|    
-      t.string   "reference"
-      t.string   "element_type"
-      t.string   "upload_content_type"
-      t.integer  "upload_file_size"
-      t.string   "upload_uid"
-      t.string   "upload_name"
-      t.text     "value"
-      t.datetime "created_at"
-      t.datetime "updated_at"
+      t.string   :reference
+      t.string   :element_type
+      t.string   :upload_content_type
+      t.integer  :upload_file_size
+      t.string   :upload_uid
+      t.string   :upload_name
+      t.text     :value
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 
