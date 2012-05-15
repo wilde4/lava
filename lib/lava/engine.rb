6815d949d33be30ab4722ce2d200064e9283289f
Gavin Laking <gavinlaking@gmail.com>
2012-05-15 15:10:43 +0100

    added: engine name

--- lib/lava/engine.rb
+++ lib/lava/engine.rb
@@ -4,6 +4,8 @@ require "simple_form"
 module Lava
 
   class Engine < Rails::Engine
+    engine_name :lava
+    
     initializer "lava.load_app_instance_data" do |app|
       Lava.setup do |config|
         config.app_root = app.root
