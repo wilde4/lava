4c654d6933629718a000ff76fc7c3c5bde357efb
Gavin Laking <gavinlaking@gmail.com>
2012-05-15 15:10:32 +0100

    fixed: namespacing of models

--- app/controllers/lava/elements_controller.rb
+++ app/controllers/lava/elements_controller.rb
@@ -4,15 +4,15 @@ module Lava
     layout false
 
     def show
-      @element = Element.find(params[:id])
+      @element = Lava::Element.find(params[:id])
     end
 
     def new
-      @element = Element.new
+      @element = Lava::Element.new
     end
 
     def create
-      @element = Element.find_or_create_element(params[:element])
+      @element = Lava::Element.find_or_create_element(params[:element])
       @element.update_attributes({:value => params[:value]}) if params[:value]
       @element.update_attributes(params[:element]) if params[:element]  
       args = {:width => params[:element][:width], :height => params[:element][:height]}
@@ -20,17 +20,17 @@ module Lava
     end
 
     def edit
-      @element = Element.find_by_instance_id(params[:instance_id])
+      @element = Lava::Element.find_by_instance_id(params[:instance_id])
     end
 
     def update
-      @element = Element.find_or_create_element(params[:element])
+      @element = Lava::Element.find_or_create_element(params[:element])
       @element.update_attributes(params[:element])
       render :partial => "lava/elements/#{@element.element_type}",  :locals => { :element => @element }
     end
 
     def destroy
-      @element = Element.find(params[:id])
+      @element = Lava::Element.find(params[:id])
       @element.destroy
       redirect_to :back
     end
