ac35b6c79f98b46a4559c4792f3ccd919210a0ea
Gavin Laking <gavinlaking@gmail.com>
2012-08-24 09:27:45 +0100

    changed namespaced url

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :elements
-  mount Lava::Engine => "/", :as => "lava" # need :as for namespacing
+  mount Lava::Engine => "/", :as => "l" # need :as for namespacing
   root :to => "pages#index"
 end
