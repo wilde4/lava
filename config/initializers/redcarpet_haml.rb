e46806f3662ac70b93dd966eafcd53fcf19375c1
Gavin Laking <gavinlaking@gmail.com>
2012-09-04 13:24:23 +0100

    updated initializer

--- config/initializers/redcarpet_haml.rb
+++ config/initializers/redcarpet_haml.rb
@@ -1,7 +1,8 @@
 module Haml::Filters::Markdown
   include Haml::Filters::Base
   lazy_require 'redcarpet'
+
   def render(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, $Application.markdown).render(text).html_safe
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, {:autolink => true, :space_after_headers => true}).render(text)
   end
 end
