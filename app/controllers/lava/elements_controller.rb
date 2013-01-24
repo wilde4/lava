31cca64198c7363076fc59b901dbfbdbbaf1bc3c
Robert Wilde <r.wilde@volcanic.co.uk>
2013-01-24 16:58:19 +0000

    alternative image upload

--- app/controllers/lava/elements_controller.rb
+++ app/controllers/lava/elements_controller.rb
@@ -26,8 +26,18 @@ module Lava
       @element = Lava::Element.find_or_create_element(params[:element])
       @element.update_attributes({:value => params[:value]}) if params[:value]
       @element.update_attributes(params[:element]) if params[:element]
-      args = {:width => params[:element][:width], :height => params[:element][:height]}
-      render :partial => "lava/elements/#{@element.element_type}",  :locals => { :element => @element, :args => args }
+      # args = {:width => params[:element][:width], :height => params[:element][:height]}
+      
+      respond_to do |format|
+        format.js {
+          render :partial => "lava/elements/#{@element.element_type}",  :locals => { :element => @element, :args => args }
+        }
+        format.html {
+          redirect_to :back
+        }
+      end
+      
+
     end
 
     def edit
