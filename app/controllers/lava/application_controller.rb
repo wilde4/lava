9bc60c98a19ea0908d4300bcd72f4e6993f4aa4f
Robert Wilde <wilde4@gmail.com>
2013-09-16 20:02:49 +0100

    edit mode

--- app/controllers/lava/application_controller.rb
+++ app/controllers/lava/application_controller.rb
@@ -2,11 +2,11 @@ module Lava
   class ApplicationController < ::ApplicationController
 
     # hook this into devise if you wish
-    def can_edit?
-      true
-      #user_signed_in? && current_user.admin? # standard affair
-    end
-    helper_method :can_edit?
+    # def can_edit?
+    #   false
+    #   #user_signed_in? && current_user.admin? # standard affair
+    # end
+    # # helper_method :can_edit?
 
   end
 end
